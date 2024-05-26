8e8db5c04524997e72c7aeb92632d109d30eec04
Ayush <dev.intern@gosharpener.com>
2024-05-26 17:36:23 +0530

    Connected to MySQL Database

--- src/main/java/productservice/models/BaseModel.java
+++ src/main/java/productservice/models/BaseModel.java
@@ -1,12 +1,19 @@
 package productservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
 @Setter
 @Getter
+@MappedSuperclass
 public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date createdDate;
     private Date updatedDate;
--- src/main/java/productservice/models/Product.java
+++ src/main/java/productservice/models/Product.java
@@ -1,15 +1,21 @@
 package productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class Product extends BaseModel{
     private String title;
     private Double price;
     private String description;
+    @ManyToOne
     private Category category;
     private String imageUrl;
+    @Transient
     private Rating rating;
 }
