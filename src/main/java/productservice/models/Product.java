a8dc0cee41a1924c188b8e795e8f940d59d4cc77
Ayush <dev.intern@gosharpener.com>
2024-05-26 20:00:25 +0530

    Bug Fix : Cascading effect on adding a product when category is not present, Infinite Recursion with JSON

--- src/main/java/productservice/models/Product.java
+++ src/main/java/productservice/models/Product.java
@@ -1,10 +1,13 @@
 package productservice.models;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 
 @Setter
 @Getter
@@ -14,6 +17,8 @@ public class Product extends BaseModel{
     private Double price;
     private String description;
     @ManyToOne
+    @Cascade(CascadeType.ALL)
+    @JsonManagedReference
     private Category category;
     private String imageUrl;
     @Transient
